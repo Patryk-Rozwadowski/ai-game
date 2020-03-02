69a3e5b722736d18ce5211b5525a83c9927f3f88
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2020-03-03 00:16:54 +0100

    Add ball collision detection

--- Ball.js
+++ Ball.js
@@ -1,30 +1,49 @@
 class Ball {
-    constructor(x = canvas.width / 2 , y = canvas.height, ballRadius = 10) {
+    constructor(x = 600, y = 600, ballRadius = 110) {
         this.x = x;
         this.y = y;
+        this.x_step = 1;
+        this.y_step = -2;
+        this.color = 'blue';
+        this.mass = this.ballRadius * this.ballRadius * this.ballRadius;
         this.ballRadius = ballRadius;
     }
 
     draw() {
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.ballRadius, 0, Math.PI * 2);
-        ctx.fillStyle = 'green';
+        ctx.fillStyle = this.color;
         ctx.fill();
         ctx.closePath();
     }
 
-    move() {
-        let x_step = 2;
-        let y_step = -2;
+    changeColor() {
+        const arr = ['blue', 'green', 'yellow', 'purple'];
+        this.color = arr[Math.floor(Math.random() * arr.length)];
+    }
 
+    collision() {
+        if (this.y + this.y_step - this.ballRadius < 0) {
+            this.y_step = -this.y_step;
+        } else if (this.x + this.x_step - this.ballRadius < 0) {
+            this.x_step = -this.x_step;
+        } else if (this.x + this.x_step + this.ballRadius > canvas.width) {
+            this.x_step = -this.x_step;
+            this.changeColor();
+        } else if (this.y + this.y_step + this.ballRadius > canvas.height) {
+            this.y_step = -this.y_step;
+        }
+    }
+
+    move() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         this.draw();
-        this.x += x_step;
-        this.y += y_step;
+        this.collision();
+        this.x += this.x_step;
+        this.y += this.y_step;
     }
 }
 
 const ball = new Ball();
 
 setInterval(() => ball.move(), 3);
-ball.draw();
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const canvas = document.getElementById('gameContainer');
+canvas.width = 1000;
+canvas.height = 900;
 const ctx = canvas.getContext('2d');
 
 const setUp = () => {
