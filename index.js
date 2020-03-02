a82df073f6ce7cb8dfca2e69ada655a8c37b2f50
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2020-03-02 22:37:41 +0100

    Init

---
+++ index.js
@@ -0,0 +1,11 @@
+const canvas = document.getElementById('gameContainer');
+const context = canvas.getContext('2d');
+
+class Player {
+    constructor(x,y) {
+        this.x = canvas.width / 3;
+        this.y = canvas.height / 2;
+    }
+
+
+}
